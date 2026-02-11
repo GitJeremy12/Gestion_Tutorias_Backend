35122d5346e3f2c047d1831e40fc06b78941eccd
Jorge Luis <joluzamce2001@gmail.com>
2026-02-11 11:15:07 -0500

    [ADD]README.md

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,8 +9,9 @@ const router = Router();
 router.post("/login",validateLogin,login);
 // CA-3.2: POST /api/register
 router.post("/register",validateRegister,register);
-
+// CA-3.3: GET /api/auth/profile
 router.get("/auth/profile", verifyToken, getProfile);
+// CA-3.4: PUT /api/auth/profile
 router.put("/auth/profile", verifyToken, updateProfile);
 
 export default router;
